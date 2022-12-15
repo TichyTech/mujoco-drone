4023ccee21ee37548e1a05583f77abcba37e5e1f
tomtch <tomtch7@gmail.com>
2022-12-15 17:50:52 +0100

    load custom model config in evaluation

--- evaluate.py
+++ evaluate.py
@@ -1,19 +1,20 @@
 from ray.rllib.algorithms.ppo import PPOConfig
 from environments.VecDrone import VecDrone, base_config
-from ray.rllib.models.catalog import MODEL_DEFAULTS
-from training import rollout_trajectory
+from training import rollout_trajectory, load_model_config
 import os
 
 model_dir = 'models/PPO/LSTM/'
-checkpoint_to_load = 'checkpoint_000010'
+checkpoint_to_load = 'checkpoints/checkpoint_000960'
 
 # environment configuration
 eval_env_config = base_config
-model_config = MODEL_DEFAULTS
-model_config = {"fcnet_hiddens": [256, 256]}
+eval_env_config['controlled'] = True
+eval_env_config['window_title'] = 'evaluation'
+model_config = load_model_config(model_dir)
 
 if __name__ == '__main__':
-    algo_config = PPOConfig()\
+    algo_config = PPOConfig() \
+        .training(model=model_config) \
         .resources(num_gpus=1)\
         .framework(framework='torch') \
         .rollouts(num_rollout_workers=0) \
