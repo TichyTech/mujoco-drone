ba5373a37f74b715af793be51a9d751eacc90680
tomtch <tomtch7@gmail.com>
2023-01-03 14:07:00 +0100

    refactor

--- training.py
+++ training.py
@@ -14,26 +14,6 @@ def save_model_config(model_dir, model_config):
         json.dump(model_config, f, indent=4)
 
 
-def rollout_trajectory(env, algo):
-    lstm = False
-    if algo.config['model']['use_lstm']:
-        lstm = True
-        state = np.zeros((2, algo.config['model']['lstm_cell_size']))  # init state to all zeros
-    obs = env.vector_reset()
-    env.render()
-    done = [False]
-    total_reward = 0.0
-    while not np.array(done).all():
-        if lstm:  # pass the state as well
-            action, state, _ = algo.compute_single_action(obs[0], state)
-        else:
-            action = algo.compute_single_action(obs[0])
-        obs, reward, done, info = env.vector_step([action])
-        env.render()
-        total_reward += reward[0]
-    print(f"Rollout total-reward={total_reward}")
-
-
 def train(algo, num_epochs, model_dir='models', checkpoint_ep=10):
     for ep in range(num_epochs):
         start = time()
@@ -44,6 +24,3 @@ def train(algo, num_epochs, model_dir='models', checkpoint_ep=10):
         algo.evaluate()
         if (ep + 1) % checkpoint_ep == 0:
             print("Saving checkpoint to {}".format(algo.save(model_dir + 'checkpoints')))  # save checkpoint
-            # for _ in range(eval_rollouts):
-                # rollout a trajectory using the learned model
-                # rollout_trajectory(eval_env, algo)
