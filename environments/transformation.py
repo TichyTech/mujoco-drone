ec87f3ce3adcfd04cee54b4027a16387dbffb419
tomtch <tomtch7@gmail.com>
2023-03-01 18:23:04 +0100

    common file for coordinate transforms

---
+++ environments/transformation.py
@@ -0,0 +1,18 @@
+from scipy.spatial.transform import Rotation as R
+import numpy as np
+
+
+def mujoco_quat2DCM(quat):
+    """convert from mujoco quaternion to rotation matrix"""
+    return R.from_quat(np.append(quat[1:], quat[0])).as_matrix()
+
+
+def mujoco_quat2rpy(quat):
+    """convert from mujoco quaternion to roll pitch and yaw angles"""
+    return R.from_quat(np.append(quat[1:], quat[0])).as_euler('ZYX')[::-1]
+
+
+def mujoco_rpy2quat(rpy):
+    """convert from roll pitch yaw angles to mujoco quaternion"""
+    quat = R.from_euler('ZYX', rpy[::-1]).as_quat()
+    return np.append(quat[3], quat[:3])
