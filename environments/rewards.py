951ffd40956e5dc903ba0ebdb06c8bd0d7c36def
tomtch <tomtch7@gmail.com>
2023-03-15 18:37:44 +0100

    new reward penalizing pendulum to reference distance instead of drone distance.

--- environments/rewards.py
+++ environments/rewards.py
@@ -1,4 +1,5 @@
 import numpy as np
+from .transformation import mujoco_rpy2quat, mujoco_quat2DCM
 
 
 def default_reward_fcn(env, state, action, num_steps):
@@ -8,7 +9,7 @@ def default_reward_fcn(env, state, action, num_steps):
     heading_err = abs((heading_err + np.pi) % (2 * np.pi) - np.pi)
     pos_err = np.linalg.norm(state[:3] - ref[:3])
     too_far = pos_err > env.max_distance
-    reward = - pos_err - 200*too_far - heading_err
+    reward = 3 - pos_err - 200*too_far - heading_err
     return reward
 
 
@@ -47,19 +48,28 @@ def reward_1(env, state, action, num_steps):
     rot_energy = (np.array(state[6:9])**2).sum()
     pendulum_energy = (np.array(state[15:18])**2).sum()
     too_far = pos_err > env.max_distance**2 - 3
-    reward = (5 + 10*close_enough -3*pos_err*(1 + num_steps/100) - 10*too_far -0.3*tilt_mag - 0.7*heading_err - 0.3*ctrl_effort - 0.3*rot_energy - 0.5*pendulum_energy)/10
+    reward = (7 + 20*close_enough -3*pos_err*(1 + num_steps/150) - 10*too_far -0.3*tilt_mag - 0.7*heading_err - 0.3*ctrl_effort - 0.3*rot_energy - 0.5*pendulum_energy)/10
     return reward
 
-def reward_2(env, state, action, num_steps):
+
+def reward_pendulum_dist(env, state, action, num_steps):
     ref = env.reference
     heading_err = np.linalg.norm(state[5] - ref[3])
     heading_err = ((heading_err + np.pi) % (2 * np.pi) - np.pi)**2
     tilt_mag = (np.array(state[3:5]) ** 2).sum()
-    pos_err = ((state[:3] - ref[:3]) ** 2).sum()
+
+    pendulum_rpy = state[12:15]
+    drone_R = mujoco_quat2DCM(mujoco_rpy2quat(state[3:6]))  # drone rotation in world frame
+    pendulum_R = mujoco_quat2DCM(mujoco_rpy2quat(pendulum_rpy))  # pendulum rotation in drone frame
+    params = state[25:]
+    pendulum_end = np.array([[0], [0], [-params[5]]])  # pendulum end in pendulum frame
+    R = drone_R @ pendulum_R
+    pendulum_pos = state[:3] + (R @ pendulum_end)[:, 0]
+    pos_err = ((pendulum_pos - ref[:3]) ** 2).sum()
     close_enough = pos_err < 0.2
     ctrl_effort = (np.array(action) ** 2).sum()
     rot_energy = (np.array(state[6:9])**2).sum()
     pendulum_energy = (np.array(state[15:18])**2).sum()
     too_far = pos_err > env.max_distance**2 - 3
-    reward = (5 + 20*close_enough -3*pos_err*(1 + num_steps/50) - 20*too_far -0.5*tilt_mag - 1*heading_err - 0.3*ctrl_effort - 0.3*rot_energy - 1*pendulum_energy)/10
+    reward = (7 + 20*close_enough -3*pos_err*(1 + num_steps/150) - 10*too_far -0.3*tilt_mag - 0.7*heading_err - 0.3*ctrl_effort - 0.3*rot_energy - 0.5*pendulum_energy)/10
     return reward
