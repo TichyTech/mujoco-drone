02ca731ef2b8483addc8d6f24f5b9f45c073a3af
tomtch <tomtch7@gmail.com>
2023-05-07 15:35:27 +0200

    refactor

--- rollout.py
+++ rollout.py
@@ -5,17 +5,17 @@ from environments.BaseDroneEnv import BaseDroneEnv, base_config
 from ray.rllib.models import ModelCatalog
 from models.PPO.RMA.RMA_model import RMA_model, RMA_model_smaller2, RMA_full
 from distributions import MyBetaDist, MySquashedGaussian
-from environments.ObservationWrappers import LocalFrameRPYParamsEnv, LocalFrameRPYFakeParamsEnv
+from environments.observation_wrappers import LocalFrameRPYParamsEnv, LocalFrameRPYFakeParamsEnv, LocalFramePRYaccEnv
 from ray.rllib.policy.policy import Policy
 from evaluation import load_policy_state
 from environments.rewards import *
 
 
-environment = LocalFrameRPYParamsEnv  # observation transform
-reward_fcn = distance_energy_reward_pendulum_angle
+environment = LocalFramePRYaccEnv  # observation transform
+reward_fcn = distance_energy_reward
 model = RMA_full
 dist = MyBetaDist
-num_drones = 64
+num_drones = 8
 
 checkpoint_dir = 'models/PPO/RMA/checkpoints/'  # directory where to look for checkpoints
 checkpoint_to_load = 'checkpoint_000050'  # saved checkpoint name
@@ -30,7 +30,9 @@ eval_env_config['max_distance'] = 3
 eval_env_config['reward_fcn'] = reward_fcn
 eval_env_config['max_steps'] = 4096
 eval_env_config['state_difficulty'] = 0.3
-eval_env_config['param_difficulty'] = 2
+eval_env_config['param_difficulty'] = 1
+eval_env_config['param_difficulty'] = 1
+eval_env_config['window_title'] = 'rollout'
 
 
 ModelCatalog.register_custom_model(model.__name__, model)
