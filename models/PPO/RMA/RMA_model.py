da1806b2a054c2f0ed0cb22e179d37c511d682a3
tomtch <tomtch7@gmail.com>
2023-05-07 15:35:51 +0200

    bug fix to RMA

--- models/PPO/RMA/RMA_model.py
+++ models/PPO/RMA/RMA_model.py
@@ -38,23 +38,19 @@ class RMA_full(TorchModelV2, nn.Module):
         self.train_adaptation = custom_config['train_adaptation']
         self.seq_len = custom_config['adapt_seq_len']
 
-        if self.train_adaptation:  # if we are training the adaptation module, we need a history of actions
-            self.view_requirements = {
-                "obs": ViewRequirement(shift="-{}:0".format(self.seq_len - 1), space=self.obs_space, batch_repeat_value=self.seq_len),
-                "prev_actions": ViewRequirement(shift="-{}:-1".format(self.seq_len), data_col='actions', space=self.action_space, batch_repeat_value=self.seq_len)
-            }
+        if self.train_adaptation and self.seq_len > 1:  # if we are training the adaptation module, we need a history of actions
+            self.view_requirements["obs_history"] = ViewRequirement(shift="%d:0" % (-self.seq_len + 1), data_col='obs', space=self.obs_space, batch_repeat_value=1)
+            self.view_requirements["action_history"] = ViewRequirement(shift="%d:-1" % (-self.seq_len), data_col='actions', space=self.action_space, batch_repeat_value=1)
         else:
-            self.view_requirements = {
-                "obs": ViewRequirement(shift=0, space=self.obs_space),
-                "prev_actions": ViewRequirement(shift=-1, data_col='actions', space=self.action_space)
-            }
+            self.view_requirements["obs_history"] = ViewRequirement(shift=0, data_col='obs', space=self.obs_space)
+            self.view_requirements["action_history"] = ViewRequirement(shift=-1, data_col='actions', space=self.action_space)
 
         self.param_encoder = nn.Sequential(
             SlimFC(in_size=self.num_params, out_size=32, initializer=nn.init.xavier_normal_, activation_fn='tanh'),
-            SlimFC(in_size=32, out_size=self.param_embed_dim, initializer=nn.init.xavier_normal_, activation_fn='tanh'),
+            SlimFC(in_size=32, out_size=self.param_embed_dim, initializer=nn.init.xavier_normal_, activation_fn=None),
         )
 
-        self.adaptation_module = TimeCNN(self.num_states + self.num_actions, self.param_embed_dim, self.seq_len)
+        self.adaptation_module = TimeCNN2(self.num_states + self.num_actions, self.param_embed_dim, self.seq_len)
 
         hidden_in_dim = self.num_states + self.num_actions + self.param_embed_dim
         self._hidden_layers = nn.Sequential(
@@ -82,23 +78,31 @@ class RMA_full(TorchModelV2, nn.Module):
 
     @override(TorchModelV2)
     def forward(self, input_dict, state, seq_lens):
-        obs = input_dict["obs"].float()
-        prev_actions = input_dict["prev_actions"].float()
+        obs_history = input_dict["obs_history"].float()
+        action_history = input_dict["action_history"].float()
+        if isinstance(input_dict, SampleBatch):
+            is_training = bool(input_dict.is_training)
+        else:
+            is_training = bool(input_dict.get("is_training", False))
+
+        self._hidden_layers.train(mode=is_training)
+
+        if self.train_adaptation and self.seq_len > 1:
+            s_a_history = torch.cat((obs_history[:, :, :self.num_states], action_history), dim=-1)  # concatenate state and action histories
+            e = obs_history[:, -1, -self.num_params:]  # current drone parameters
+            flat_in = s_a_history[:, -1]  # take current state and last actions only
+        else:
+            s_a_history = torch.cat((obs_history[:, :self.num_states], action_history), dim=-1)  # concatenate state and action histories
+            e = obs_history[:, -self.num_params:]  # current drone parameters
+            flat_in = s_a_history
 
         if self.train_adaptation:
-            s_a_history = torch.cat((obs[:, :, :self.num_states], prev_actions), dim=-1)  # concatenate state and action histories
             self.z_hat = self.adaptation_module(s_a_history)
             with torch.no_grad():
-                e = obs[:, 0, self.num_states:self.num_states + self.num_params]  # flattened drone parameters
                 self.z = self.param_encoder(e)  # encode drone parameters
-                self._features = self._hidden_layers(torch.cat((s_a_history[:, -1], self.z), dim=-1))
+                self._features = self._hidden_layers(torch.cat((flat_in, self.z_hat), dim=-1))
                 logits = self._logits(self._features)
         else:
-            obs = obs.reshape(obs.shape[0], -1)
-            prev_actions = prev_actions.reshape(prev_actions.shape[0], -1)
-            flat_in = torch.cat((obs[:, :self.num_states], prev_actions), dim=-1)
-
-            e = obs[:, self.num_states:self.num_states + self.num_params]  # flattened drone parameters
             self.z = self.param_encoder(e)  # encode drone parameters
             self._features = self._hidden_layers(torch.cat((flat_in, self.z), dim=-1))
             logits = self._logits(self._features)
@@ -114,12 +118,12 @@ class RMA_full(TorchModelV2, nn.Module):
 
     def custom_loss(self, policy_loss, loss_inputs):
         # weight decay loss - gets added to ppo loss
-        wd = 1e-4
+        wd = 1e-5
         if self.train_adaptation:  # adaptation loss
             l = self.adaptation_loss(self.z_hat, self.z)
-            self.al = l.cpu().item()
             for p in self.adaptation_module.parameters():
                 l += wd * torch.norm(p) ** 2
+            self.al = l.cpu().detach().numpy()
             return [l]
         for p in self.parameters():
             policy_loss[0] += wd*torch.norm(p)**2
@@ -150,7 +154,36 @@ class TimeCNN(nn.Module):
 
         self.outMLP = nn.Sequential(
             SlimFC(in_size=test_x.size(1), out_size=32, initializer=nn.init.xavier_normal_, activation_fn='tanh'),
-            SlimFC(in_size=32, out_size=param_embed_dim, initializer=nn.init.xavier_normal_, activation_fn='tanh')
+            SlimFC(in_size=32, out_size=param_embed_dim, initializer=nn.init.xavier_normal_, activation_fn=None)
+        )
+
+    def forward(self, x):
+        y = self.inMLP(x)
+        y = self.tCNN(y.transpose(1, 2))
+        y = self.outMLP(y.flatten(1))
+        return y
+
+
+class TimeCNN2(nn.Module):
+    def __init__(self, in_feature_dim, param_embed_dim, seq_len=50):
+        nn.Module.__init__(self)
+
+        self.inMLP = nn.Sequential(
+            SlimFC(in_size=in_feature_dim, out_size=32, initializer=nn.init.xavier_normal_, activation_fn='tanh'),
+            SlimFC(in_size=32, out_size=32, initializer=nn.init.xavier_normal_, activation_fn='tanh'),
+            SlimFC(in_size=32, out_size=32, initializer=nn.init.xavier_normal_, activation_fn='tanh')
+        )
+
+        self.tCNN = nn.Sequential(
+            nn.Conv1d(32, 32, 5, 2),
+            nn.Conv1d(32, 16, 5)
+        )
+
+        test_x = self.tCNN(torch.zeros((1, 32, seq_len))).flatten(1)
+
+        self.outMLP = nn.Sequential(
+            SlimFC(in_size=test_x.size(1), out_size=64, initializer=nn.init.xavier_normal_, activation_fn='tanh'),
+            SlimFC(in_size=64, out_size=param_embed_dim, initializer=nn.init.xavier_normal_, activation_fn=None)
         )
 
     def forward(self, x):
